7daf2ebaeadd1494783e963ae187bc3129366ba2
VinishaGupta <vinishasgupta@gmail.com>
2026-04-14 15:41:41 +0530

    shift zeros to end

--- array/src/ShiftZerosToEnd.java
+++ array/src/ShiftZerosToEnd.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class ShiftZerosToEnd {
     public static void main(String[] args) {
         int[] arr={0,2,1,5,0};
-        fun(arr);
+//        fun(arr);
         fun2(arr);
     }
 
