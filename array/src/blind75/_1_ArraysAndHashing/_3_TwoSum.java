ecaab7a6595114161c27ac9720d32af0aed871bf
Vinisha Gupta <vinishasgupta@gmail.com>
2026-05-26 23:11:52 +0530

    1-arrays and hashing -------- 3. two sum

---
+++ array/src/blind75/_1_ArraysAndHashing/_3_TwoSum.java
@@ -0,0 +1,23 @@
+package blind75._1_ArraysAndHashing;
+
+import java.util.Arrays;
+
+public class _3_TwoSum {
+    public static void main(String[] args) {
+        int[] arr={2,7,9,19};
+        int target=78;
+        System.out.println(Arrays.toString(twoLoops(arr,target)));
+    }
+
+    public static int[] twoLoops(int[] arr,int target){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i]+arr[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+
+        return new int[]{-1,-1};
+    }
+}
