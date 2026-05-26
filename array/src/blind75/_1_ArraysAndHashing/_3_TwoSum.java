df5dabb0d71df3be4bc5c2cb3c4caab9fd82bf10
Vinisha Gupta <vinishasgupta@gmail.com>
2026-05-26 23:12:53 +0530

    1-arrays and hashing -------- 3. two sum

--- array/src/blind75/_1_ArraysAndHashing/_3_TwoSum.java
+++ array/src/blind75/_1_ArraysAndHashing/_3_TwoSum.java
@@ -9,6 +9,9 @@ public class _3_TwoSum {
         System.out.println(Arrays.toString(twoLoops(arr,target)));
     }
 
+//    //    Time complexity: O(n^2)
+////    Space complexity: O(1)
+
     public static int[] twoLoops(int[] arr,int target){
         for (int i = 0; i < arr.length; i++) {
             for (int j = i+1; j < arr.length; j++) {
