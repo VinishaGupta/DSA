2719ba8afdca524b8963699c2839a08dd346f234
Vinisha Gupta <vinishasgupta@gmail.com>
2026-07-08 02:29:04 +0530

    pattern 3

--- array/src/Patterns/Main.java
+++ array/src/Patterns/Main.java
@@ -2,7 +2,7 @@ package Patterns;
 
 public class Main {
     public static void main(String[] args) {
-        pattern2(4);
+        pattern3(5);
     }
 
     public static void pattern1(int n){
@@ -18,6 +18,15 @@ public class Main {
         for (int i = 1; i <=n; i++) {
             for (int j = 1; j <=i; j++) {
                 System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern3(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j);
             }
             System.out.println();
         }
