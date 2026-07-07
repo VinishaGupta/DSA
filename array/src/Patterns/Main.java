691fb93bca0e12a9d6e2fa5682a08da9c4706697
Vinisha Gupta <vinishasgupta@gmail.com>
2026-07-08 02:27:21 +0530

    pattern 2

--- array/src/Patterns/Main.java
+++ array/src/Patterns/Main.java
@@ -2,13 +2,22 @@ package Patterns;
 
 public class Main {
     public static void main(String[] args) {
-        print(4);
+        pattern2(4);
     }
 
-    public static void print(int n){
+    public static void pattern1(int n){
         for (int i = 1; i <=n; i++) {
             for (int j = 1; j <=n; j++) {
                 System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern2(int n){
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
