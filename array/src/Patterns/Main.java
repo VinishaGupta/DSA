b092f2d210fa463a7070ae9a65fca6084c62982c
Vinisha Gupta <vinishasgupta@gmail.com>
2026-07-08 02:50:45 +0530

    pattern 6

--- array/src/Patterns/Main.java
+++ array/src/Patterns/Main.java
@@ -2,7 +2,7 @@ package Patterns;
 
 public class Main {
     public static void main(String[] args) {
-        pattern5(5);
+        pattern6(5);
     }
 
     public static void pattern1(int n){
@@ -52,12 +52,16 @@ public class Main {
 
     public static void pattern6(int n){
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <=i ; j++) {
-                System.out.print(i);
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(' ');
+            }
+            for (int j = 1; j <=2*i-1 ; j++) {
+                System.out.print('*');
             }
             System.out.println();
         }
     }
+
 
 
 }
