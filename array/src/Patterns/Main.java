fad4f476182a073bc3440f5977b47d347750f229
Vinisha Gupta <vinishasgupta@gmail.com>
2026-07-08 02:32:35 +0530

    pattern 4

--- array/src/Patterns/Main.java
+++ array/src/Patterns/Main.java
@@ -31,5 +31,14 @@ public class Main {
             System.out.println();
         }
     }
+
+    public static void pattern4(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
 }
 
