3486e5b262e97545224b237c3e61cb2548811f8a
Vinisha Gupta <vinishasgupta@gmail.com>
2026-07-09 13:34:39 +0530

    pattern 19

--- array/src/Patterns/Main.java
+++ array/src/Patterns/Main.java
@@ -2,7 +2,7 @@ package Patterns;
 
 public class Main {
     public static void main(String[] args) {
-        pattern16(5);
+        pattern19(5);
     }
 
     public static void pattern1(int n){
@@ -220,6 +220,49 @@ public class Main {
                 System.out.print(ch2);
                 ch2--;
             }
+            System.out.println();
+        }
+    }
+
+//    public static void pattern17(int n){
+//        for (int i = n; i ; i++) {
+//
+//        }
+//    }
+
+    public static void pattern18(int n) {
+        for (int i = 1; i <= 2 * n; i++) {
+
+            int stars = i <= n ? n - i + 1 : i - n;
+            int spaces = i <= n ? 2 * i - 2 : 2 * (2 * n - i);
+
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void pattern19(int n) {
+        for (int i = 1; i <=n; i++) {
+
+            for (int j = 1; j <=n; j++) {
+                if(i==1  || i==n || j==1 || j==n){
+                    System.out.print('*');
+                } else{
+                    System.out.print(' ');
+                }
+            }
+
             System.out.println();
         }
     }
